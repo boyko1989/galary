6cb79b9681fd0cf6482e4eee98f402ab9585d619
boyko1989 <hunwiss89@gmail.com>
2020-10-08 17:35:29 +0400

    Рабочая пагинация с перезагрузкой страницы

--- function.php
+++ function.php
@@ -12,13 +12,65 @@ function get_images($dir) {
 */
 
 function get_images($dir) {
-    $files = scandir($dir);
-    /*
+    @$files = scandir($dir);
+    if(!$files) return false;
+    $pattern = '#\.(jpe?g|png|gif)$#i';
     foreach ($files as $key => $file) {
-        if ($file == '.' || $file == '..') unset($files[$key]);
-    }*/
-    unset($files[0], $files[1]);
+        if (!preg_match($pattern, $file)) {
+            unset($files[$key]);
+        }
+    }
+    $files = array_merge($files);
     return $files;
+}
+
+# пагинация
+function pagination($page, $count_pages) {
+    // << < 3 4 5 6 7 > >>
+    // $back - ссылка назад
+    // $forward - ссылка вперёд
+    // $startpage - ссылка в начало
+    // $endpage - ссылка в конец
+    // $page2left - вторая страница слева
+    // $page1left - первая страница сдева
+    // $page2right - вторая страница справа
+    // $page1right - первая страница справа
+
+    $uri = "?";
+
+    //если есть параметры в адресной строке
+    if ($_SERVER['QUERY_STRING']) {
+        foreach ($_GET as $key => $value) {
+            if ($key != 'page') $uri .= "{$key}=$value&amp;";
+        }
+    }
+
+    if ($page > 1) {
+        $back = "<a class='nav-link' href='{$uri}page=".($page - 1)."'>&lt;</a>";
+    }
+    if ($page < $count_pages) {
+        $forward = "<a class='nav-link' href='{$uri}page=".($page + 1)."'>&gt;</a>";
+    }
+    if ($page > 3) {
+        $startpage = "<a class='nav-link' href='{$uri}page=1'>&lt;&lt;</a>";
+    }
+    if ($page < ($count_pages - 2)) {
+        $endpage = "<a class='nav-link' href='{$uri}page={$count_pages}'>&gt;&gt;</a>";
+    }
+    if ($page - 2 > 0) {
+        $page2left = "<a class='nav-link' href='{$uri}page=".($page-2)."'>" .($page-2). "</a>";
+    }
+    if( $page - 1 > 0 ){
+		$page1left = "<a class='nav-link' href='{$uri}page=" .($page-1). "'>" .($page-1). "</a>";
+	}
+	if( $page + 1 <= $count_pages ){
+		$page1right = "<a class='nav-link' href='{$uri}page=" .($page+1). "'>" .($page+1). "</a>";
+	}
+	if( $page + 2 <= $count_pages ){
+		$page2right = "<a class='nav-link' href='{$uri}page=" .($page+2). "'>" .($page+2). "</a>";
+	}
+
+    return $startpage." ".$back." ".$page2left." ".$page1left." ".'<a class="nav-active">'.$page.'</a>'." ".$page1right." ".$page2right." ".$forward." ".$endpage;
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,32 @@
     require_once 'function.php';
     $dir = 'img/';
     $images = get_images($dir);
+
+    # пагинация
+    //количество фото на страницу
+        $perpage = 9;
+    //общее количество картинок
+        $count_img = count($images);
+    //необходимое количество страниц
+        $count_pages = ceil($count_img / $perpage);
+    //если число страниц рвно нулю, приравнять его единице
+        if(!$count_pages) $count_pages = 1;
+    // получить номер запрошенной страницы
+        if(isset($_GET['page']) ) {
+            $page = (int)$_GET['page'];
+            if($page < 1) $page = 1;
+        } else {
+            $page = 1;
+        }
+    //если запрошенная страница больше максимума
+        if($page > $count_pages) $page = $count_pages;
+
+    // первая картинка на странице
+        $start_pos = ($page - 1) *$perpage;
+    // последняя картинка на странице
+        $end_pos = $start_pos + $perpage;
+        if ($end_pos > $count_img) $end_pos = $count_img; 
+    $pagination = pagination($page, $count_pages);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -14,17 +40,25 @@
 </head>
 <body>
     <div class="wrapper">
-        <h1>Галерея</h1>
         <div class="gallery">
-            <?php foreach($images as $image):?>
+        <?php if($images): $i = $start_pos + 1; ?>
+        <?php for($j = $start_pos; $j < $end_pos; $j++):?>
             <div class="item">
                 <div>
-                    <a data-lightbox="lightbox" href="<?=$dir . $image?>">
-                        <img src="<?=$dir . $image?>" alt="1" class="front">
+                    <a data-lightbox="lightbox" href="<?=$dir . $images[$j]?>">
+                        <img src="<?=$dir . $images[$j]?>" alt="1" class="front">
                     </a>
                 </div>
             </div>
-            <?php endforeach;?>
+            <?php $i++; endfor;?>
+        <?php else: ?>
+            <p>В данной галерее нет картинок</p>
+        <?php endif;?>
+
+            <?php if ($count_pages > 1): ?>
+                <div class="pagination"><?=$pagination ?></div>
+            <?php endif; ?>
+
         </div>
     </div>
 
